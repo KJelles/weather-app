ee38928ce31d6f4cbd91b8dcfcc461198874be3c
KJelles <kevinjelles98@gmail.com>
2022-11-18 21:09:13 +0100

    Reverted change

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const WEATHER_API_URL='https://api.openweathermap.org/data/2.5/';
 export const GEO_API_URL='http://api.openweathermap.org/geo/1.0/direct';
-export const WEATHER_API_KEY='9647bdbc625b478dbb78e64897fdd688';
+export const WEATHER_API_KEY='API_KEY_HERE';
